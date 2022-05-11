5224c9b958b78433ad0e5da03d4a6dd2372d5a7f
Maggie Clerkin <mclerkin92@gmail.com>
2022-05-11 13:00:47 -0400

    Updated JS code using Beautify

--- designs.js
+++ designs.js
@@ -13,30 +13,30 @@ const refresh = document.querySelector("#refresh");
 const gridInstructions = document.querySelector("#instructions");
 
 // When size is submitted by the user, call makeGrid() and display grid's instructions and refresh info
-sizePicker.addEventListener("submit", function(event){
-    event.preventDefault();
-    makeGrid();
-    refresh.classList.remove("display-none");
-    gridInstructions.classList.remove("display-none");
+sizePicker.addEventListener("submit", function(event) {
+	event.preventDefault();
+	makeGrid();
+	refresh.classList.remove("display-none");
+	gridInstructions.classList.remove("display-none");
 });
 
 //  makeGrid() function
-function makeGrid (){
-    grid.innerHTML="";
-    for (let r = 0; r < height.value; r++ ) {
-        const row = document.createElement("tr");
-        for (let c = 0; c < width.value; c++){
-            const cell = document.createElement("td");
-            row.appendChild(cell);
-        }
-        grid.appendChild(row);
-    //click to paint cells
-    grid.addEventListener("click", function(event){
-        event.target.style.backgroundColor = color.value;
-    })
-    //double click to remove paint from cells
-    grid.addEventListener("dblclick", function(event){
-        event.target.style.backgroundColor = "white";
-    })
-    }
+function makeGrid() {
+	grid.innerHTML = "";
+	for (let r = 0; r < height.value; r++) {
+		const row = document.createElement("tr");
+		for (let c = 0; c < width.value; c++) {
+			const cell = document.createElement("td");
+			row.appendChild(cell);
+		}
+		grid.appendChild(row);
+		//click to paint cells
+		grid.addEventListener("click", function(event) {
+			event.target.style.backgroundColor = color.value;
+		})
+		//double click to remove paint from cells
+		grid.addEventListener("dblclick", function(event) {
+			event.target.style.backgroundColor = "white";
+		})
+	}
 }
